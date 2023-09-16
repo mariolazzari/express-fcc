7bf5d365fff43ba269a2ca2c023289ed2740457d
Mario Lazzari <mario.lazzari@gmail.com>
2023-09-16 09:47:08 +0200

    Done lesson 6

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let app = express();
 const { join } = require("path");
+require("dotenv").config();
 
 // console.log("Hello World");
 const publicPath = join(__dirname, "public");
@@ -14,6 +15,11 @@ app.get("/", (_req, res) => {
 
 app.get("/json", (_req, res) => {
   const obj = { message: "Hello json" };
+  obj.message =
+    process.env.MESSAGE_STYLE === "uppercase"
+      ? obj.message.toUpperCase()
+      : obj.message.toLowerCase();
+
   res.json(obj);
 });
 
