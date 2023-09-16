01dc3c994b05a114c5c2e143bf5841f9ee60b8ca
Mario Lazzari <mario.lazzari@gmail.com>
2023-09-16 09:23:12 +0200

    Done lesson 3

--- myApp.js
+++ myApp.js
@@ -1,10 +1,13 @@
 let express = require("express");
 let app = express();
+const { join } = require("path");
 
-console.log("Hello World");
+// console.log("Hello World");
 
 app.get("/", (_req, res) => {
-  res.send("Hello Express");
+  // res.send("Hello Express");
+  const file = join(__dirname, "views/index.html");
+  res.sendFile(file);
 });
 
 module.exports = app;
